54bc221593370a6bc0718da16c1de64631928ed6
Michael Kebe <michael.kebe@gmail.com>
2016-09-11 07:34:13 +0200

    a failing test

--- spec/greeter_spec.js
+++ spec/greeter_spec.js
@@ -4,4 +4,8 @@ describe("greeter", function() {
   it("can be instantiated", function() {
     new Greeter();
   });
+  it("can greet", function() {
+    var greeter = new Greeter();
+    expect(greeter.greet()).toBe("Hello!");
+  });
 });
